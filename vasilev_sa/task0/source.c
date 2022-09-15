8bdc25aaec62ea9c6426001a0de9bfc26fbea59b
venn2713 <venn2713@gmail.com>
2022-09-16 01:58:54 +0300

    Final version

--- vasilev_sa/task0/source.c
+++ vasilev_sa/task0/source.c
@@ -11,27 +11,26 @@ int findMin(int x, int y)
     return (x < y) ? x : y;
 }
 
-void main()
+int main()
 {
     int x1, y1, r1, x2, y2, r2, rmax, rmin;
-    double ditance;
+    double distance;
     printf("Enter the coordinates and radius of the first circle ");
     scanf_s("%d %d %d", &x1, &y1, &r1);
     printf("Enter the coordinates and radius of the second circle ");
     scanf_s("%d %d %d", &x2, &y2, &r2);
     rmax = findMax(r1, r2);
     rmin = findMin(r1, r2);
-    ditance = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
-
-    if ((ditance > rmax + rmin) || (ditance + rmin < rmax))
+    distance = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
+    if ((distance > rmax + rmin) || (distance < rmax - rmin))
     {
         printf("0 crossings\n");
     }
-    else if ((ditance == 0) && (rmax == rmin))
+    else if ((distance == 0) && (rmax == rmin))
     {
         printf("Conside\n");
     }
-    else if ((ditance == rmax + rmin) || (ditance + rmin == rmax))
+    else if ((distance == rmax + rmin) || (distance == rmax - rmin))
     {
         printf("1 crossing\n");
     }
@@ -40,4 +39,5 @@ void main()
         printf("2 crossings\n");
     }
     system("pause");
+    return 0;
 }
