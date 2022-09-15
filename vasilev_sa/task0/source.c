2d22981944f160e99d498f01d03887083bfbd494
venn2713 <venn2713@gmail.com>
2022-09-16 00:34:53 +0300

    More functions

--- vasilev_sa/task0/source.c
+++ vasilev_sa/task0/source.c
@@ -1,33 +1,35 @@
 #include <stdio.h>
 #include <math.h>
-#include <Windows.h> 
+
+int findMax(int x, int y) 
+{
+    return (x > y) ? x : y;
+}
+
+int findMin(int x, int y)
+{
+    return (x < y) ? x : y;
+}
+
 void main()
 {
-    SetConsoleOutputCP(1251);
     int x1, y1, r1, x2, y2, r2, rmax, rmin;
     double ditance;
     printf("Enter the coordinates and radius of the first circle ");
     scanf_s("%d %d %d", &x1, &y1, &r1);
     printf("Enter the coordinates and radius of the second circle ");
     scanf_s("%d %d %d", &x2, &y2, &r2);
-    if (r1 > r2)
-    {
-        rmax = r1;
-        rmin = r2;
-    }
-    else
-    {
-        rmax = r2;
-        rmin = r1;
-    }
+    rmax = findMax(r1, r2);
+    rmin = findMin(r1, r2);
     ditance = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
+
     if ((ditance > rmax + rmin) || (ditance + rmin < rmax))
     {
         printf("0 crossings\n");
     }
-    else if ((ditance == 0) && (r1 == r2))
+    else if ((ditance == 0) && (rmax == rmin))
     {
-        printf("Сoincide\n");
+        printf("Conside\n");
     }
     else if ((ditance == rmax + rmin) || (ditance + rmin == rmax))
     {
