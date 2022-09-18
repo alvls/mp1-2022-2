1e42b95964a5d1961bca8ac4b45988e7199d8c81
Иван Капустин <ivankapustin60@gmail.com>
2022-09-18 19:43:48 +0300

    zadanie 0

---
+++ kapustin_ia/task0/source.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<math.h>
+void main()
+{
+	double x1, x2, y1, y2, r1, r2;
+	double dist;
+	printf(" VVEDITE x1,y1,x2,y2,r1,r2, gde r1 > r2");
+	scanf("%lf%lf%lf%lf%lf%lf", &x1, &y1, &x2, &y2, &r1, &r2);
+	dist = sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+	if (dist > r1 + r2)
+		printf("ne peresekautcia");
+	else if (dist < r1 - r2)
+		printf("odna vnutri drugoi,tochek kasania net");
+	else if (dist = r1 - r2)
+		printf("odna vnutri drugoi, vnutrennee kasanie");
+	else if (dist = r1 + r2)
+		printf("vneshnee kasanie");
+	else if ((dist < r1 + r2) && (dist > r1 - r2))
+		printf("Dve tochki peresecheniya");
+	else
+		printf("chto-to ne tak");
+	system("pause");
+}
