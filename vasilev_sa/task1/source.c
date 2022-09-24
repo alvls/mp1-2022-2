b76eb07c6c136bfa9e3e21c835092c4f0624cd09
venn2713 <venn2713@gmail.com>
2022-09-24 12:17:56 +0300

    Corrected inaccuracies

--- vasilev_sa/task1/source.c
+++ vasilev_sa/task1/source.c
@@ -2,6 +2,7 @@
 #include <math.h>
 #include <locale.h>
 
+int Count_of_shelfs(float start_h, float t5);
 int Count_of_shelfs(float start_h, float t5)
 {
 	float count = 0, all_height = 40;
@@ -18,41 +19,37 @@ void main()
 	// локализация
 	setlocale(LC_ALL, "Russian");
 	// объявление переменных
-	float h = 0, w = 0, d = 0, t1 = 0.5, t2 = 1.5 , t3 = 1.5, t4 = 1, t5 = 0;
-	double value_1, value_2, value_3, value_4, value_5, all_value;
-	double material_DSP = 0.00075, material_DVP = 0.0004, material_wood = 0.00065;
 	int count;
+	double h = 0, w = 0, d = 0;
+	double value_1, value_2, value_3, value_4, value_5, all_value;
+	const double t1 = 0.5, t2 = 1.5 , t3 = 1.5, t4 = 1, t5 = 1.5;
+	const double material_DSP = 0.00075, material_DVP = 0.0004, material_wood = 0.00065;
 	// ввод значений с непринятием неверных
 	printf("Необходимо внести параметры изделия:\n(если значения вне диапазона, то программа потребует ввести другие)\n");
 	while ((h < 180)||(h > 220))
 	{
 		printf("Введите высоту от 180 до 220 (в см): ");
-		scanf_s("%f", &h);
+		scanf_s("%lf", &h);
 	}
 	while ((w < 80)||(w > 120))
 	{
 		printf("Введите ширину от 80 до 120 (в см): ");
-		scanf_s("%f", &w);
+		scanf_s("%lf", &w);
 	}
 	while ((d < 50)||(d > 90))
 	{
 		printf("Введите глубину от 50 до 90 (в см): ");
-		scanf_s("%f", &d);
-	}
-	while ((t5 <= 0) || (t5 > 10))
-	{
-		printf("Введите толщину полок до 10 (в см): ");
-		scanf_s("%f", &t5);
+		scanf_s("%lf", &d);
 	}
 	// вычисление объёмов
 	count = Count_of_shelfs(h, t5);
-	value_1 = h * w * t1;								// задняя стенка из ДВП
-	value_2 = 2 * (h * d * t2);							// боковые стенки из ДСП
-	value_3 = 2 * (w * d * t3);							// крышки из ДСП
-	value_4 = h * w * t4;								// двери из дерева
-	value_5 = (w * d * t5) * count;						// внутренние полки из ДСП
+	value_1 = (h * w * t1) * material_DVP;								// задняя стенка из ДВП
+	value_2 = 2 * (h * d * t2) * material_DSP;							// боковые стенки из ДСП
+	value_3 = 2 * (w * d * t3) * material_DSP;							// крышки из ДСП
+	value_4 = (h * w * t4) * material_wood;								// двери из дерева
+	value_5 = (w * d * t5) * count * material_DSP;						// внутренние полки из ДСП
 	// весь объём
-	all_value = material_DVP * (value_1) + material_DSP * (value_2 + value_3 + value_5) + material_wood * (value_4);
+	all_value = value_1 + value_2 + value_3 + value_4 + value_5;
 	// вывод ответа
 	printf("\nИтого:\nПолок %d, ", count);
 	printf("вес шкафа %.2lf кг \n(при плостности ДСП = 750 кг/кубометр, ДВП = 400 кг/кубометр и дерева = 650 кг/кубометр)\n\n", all_value);
