533fd70a3a1cba3fa1f794497208460297b5d975
ermilovad <darya.ermilova04@inbox.ru>
2022-09-25 12:53:44 +0300

    2

--- Ermilova_DS/task1/source.c
+++ Ermilova_DS/task1/source.c
@@ -1,31 +1,29 @@
 #include <stdio.h>
-#include <locale.h>
 
 int main()
 {
-	setlocale(LC_ALL, "Rus");
-	double h, w, d, massa, t1, t2, t3, back_wall, side_walls, lids, doors, shelves;
+	double h, w, d, mass, t1, t2, t3, back_wall, side_walls, lids, doors, shelves;
 	int cnt, p1, p2, p3;
-	t1 = 0.005; // t1, t2, t3 - толщина  в метрах
+	t1 = 0.005; // t1, t2, t3 - thickness in meters
 	t2 = 0.015;
 	t3 = 0.01;
-	p1 = 800; // примерная плотность ДСП (кг/м2)
-	p2 = 820; // примерная плотность ДВП (кг/м2)
-	p3 = 600; // примерная плотность дерева (кг/м2)
-	printf("Введите размер шкафа (высоту, ширину и глубину(в метрах))");
+	p1 = 800; // approximate density of wood chipboard (kg/m2)
+	p2 = 820; // approximate density of fiberboard (kg/m2)
+	p3 = 600; // approximate density of wood (kg/m2)
+	printf("enter the wardrobe size (height, width and depth (in meters))");
 	scanf_s("%lf %lf %lf", &h, &w, &d);
 	if ((h > 1.8) && (h < 2.2) && (w > 0.8) && (w < 1.2) && (d > 0.5) && (d < 0.9)) {
-		cnt = (h / 0.4); // количество полок
-		back_wall = h * w * t1; // объемы деталей
+		cnt = (h / 0.4); // amount of shelves
+		back_wall = h * w * t1; // volumes of details
 		side_walls = 2 * h * d * t2;
 		lids = 2 * w * d * t2;
 		doors = 2 * h * w * t3;
 		shelves = cnt * d * w * t2;
-		massa = (back_wall * p2) + (side_walls * p1) + (lids * p1) + (doors * p3) + (shelves * p1);
-		printf("Масса шкафа равна %lf", massa);
+		mass = (back_wall * p2) + (side_walls * p1) + (lids * p1) + (doors * p3) + (shelves * p1);
+		printf("wardrobe mass -  %lf", mass);
 	}
 	else {
-		printf("Неверные данные");
+		printf("Wrong data");
 	}
 	system("pause");
 }
