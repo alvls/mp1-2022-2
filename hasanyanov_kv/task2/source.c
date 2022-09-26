4a6efdf3ebca88492fd3bbbaa817f921734e9bf1
Кирилл Хасаньянов <hasanyanovk2@mail.ru>
2022-09-26 23:07:03 +0300

    complete task 2

---
+++ hasanyanov_kv/task2/source.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <time.h>
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int kpop = 0, i, n, win = 0, mode; // i - Загаданное число, n - предположение, mode - режим игры, kpop - количество попыток.
+	printf("Введите режим игры:\n1 - Вы угадываете число\n2 - Программа угадывает число\n");
+	scanf_s("%d", &mode);
+	if (mode == 1)
+	{
+		srand(time(NULL)); // Для перебора значений введём рандомайзер.
+		i = (rand() % 1000) + 1; 
+		printf("Если сдаётесь, введите значение '1001'\n");
+		printf("Угадайте число (от 1 до 1000)\n");
+		while (win != 1)
+		{
+			kpop++;
+			scanf_s("%d", &n); // Если нет времени угадывать, но надо проверить работу программу.
+			if (n == 1001)
+				printf("%d\n", i);
+			else if (n > i)
+				printf("Загаданноне число меньше\n");
+			else if (n < i)
+				printf("Загаданное число больше\n");
+			else if (n == i) {
+				printf("Угадали!!!");
+				win = 1;
+			}
+		}
+		printf("Вы выиграли, число попыток:" "%d", kpop);
+		system("PAUSE");
+	}
+	else if (mode == 2)
+	{
+		char znak;
+		int min = 0, max = 1000; // Будем после каждого предположения ограничивать диапазон значения, пока не получим нужный ответ.
+		printf("Загадайте число от 1 до 1000, а я попробую его отгадать\n");
+		srand(time(NULL)); // Для перебора значений введём рандомайзер.
+		scanf_s("%c", &znak);
+		while (win != 1)
+		{
+			i = rand() % ((max - min) + 1) + min;
+			printf("%d?\n", i);
+			scanf_s(" %c", &znak);
+			if (znak == '>')
+			{
+				kpop++;
+				min = i;
+			}
+			else if (znak == '<')
+			{
+				kpop++;
+				max = i;
+			}
+			else if (znak == '=')
+			{
+				kpop++;
+				win = 1;
+			}
+			else
+			{
+				printf("Неверные данные!\n");
+				system("PAUSE");
+			}
+		}
+		printf("Я выиграл!!! Количество попыток:" "%d\n", kpop);
+		system("PAUSE");
+	}
+	return 0;
+}
