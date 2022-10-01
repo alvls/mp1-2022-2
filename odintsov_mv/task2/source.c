47f9d8bb838b66df57632ffd751e0e713841bd53
odincovm <odincovmisa16@gmail.com>
2022-10-01 11:56:08 +0300

    task 2

--- odintsov_mv/task2/source.c
+++ odintsov_mv/task2/source.c
@@ -5,10 +5,10 @@
 void main() {
 	int rezim;
 	int r1_chislo;
-	int r1_ans=0, r2_ans=500;
+	int r1_ans = 0, r2_ans;
 	int r1_counter = 0, r2_counter = 0;
 	char us_ans;
-	int flag = 1;
+	int left = 0, right = 1000;
 	srand(time(NULL));
 	setlocale(LC_ALL, "Russian");
 	printf("Введите режим 1 или 2 \n");
@@ -19,9 +19,9 @@ void main() {
 		while (1) {
 			printf("Введите число \n");
 			scanf_s("%i", &r1_ans);
-			r1_counter ++;
+			r1_counter++;
 			if (r1_ans == r1_chislo) {
-				printf("Ты победил. Количество ходов = %i",r1_counter);
+				printf("Ты победил. Количество ходов = %i", r1_counter);
 				break;
 			}
 			if (r1_ans > r1_chislo) {
@@ -34,42 +34,27 @@ void main() {
 	}
 	else {
 		printf("Загадай число \n");
-		while (flag) {
-			printf("121212");
-			r2_counter++;
-			scanf_s("%c", &us_ans);
-			if (us_ans == '=' ) {
-				printf("Число угадано. Количесво ходов = %i", r2_counter);
-				flag = 0;
+		while (1) {
+			if ((right-left)%2==0){
+				r2_ans = (right + left) / 2;
+			}
+			else {
+				r2_ans = (right + 1 + left) / 2;
+			}
+			printf("Введите 1, если загаданное число больше, 2, если загаданное число меньше и 3 если он равно числу %i", r2_ans);
+			scanf_s(" %c",&us_ans);
+			r2_counter += 1;
+			if (us_ans == '='  ) {
+				printf("Победа, количество ходов = %i", r2_counter);
+				break;
 			}
 			if (us_ans == '<') {
-				if ((r2_ans % 2) == 0) {
-					r2_ans -=r2_ans / 2;
-				}
-				else {
-					r2_ans -= (r2_ans + 1) / 2;
-				}
+				right = r2_ans;
 			}
 			if (us_ans == '>') {
-				if ((r2_ans % 2) == 0) {
-					r2_ans += (1000-r2_ans) / 2;
-				}
-				else {
-					r2_ans += (1001-r2_ans ) / 2;
-				}
+				left = r2_ans;
 			}
 		}
 	}
-
-
-
-
-
-
-
-
-
-
-	system("pause");
-
+system("pause");
 }
