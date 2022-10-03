26fac94bdbae62ae8cbbcd62a679823628e32ecd
venn2713 <venn2713@gmail.com>
2022-10-04 00:02:51 +0300

    Modified version

--- vasilev_sa/task3/source.c
+++ vasilev_sa/task3/source.c
@@ -4,78 +4,126 @@
 #include <math.h>
 
 
+int check_conditions(int input_number, short n)
+{
+    short array[5] = { 0 };
+    if (input_number < 0)
+        return 0;
+    if ((input_number < pow(10, n - 1)) || (input_number >= pow(10, n)))
+        return 1;
+    for (int k = 0; k < n; k++)
+    {
+        array[n - k - 1] = input_number % 10;
+        input_number /= 10;
+    }
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if ((array[i] == array[j]) & (i != j))
+                return 1;
+        }
+    }
+    if (array[0] == 0)
+        return 1;
+    return 0;
+}
+
 void main()
 {
     setlocale(LC_ALL, "Russian");
-    srand(time(0));
     //обьявление переменных и констант
-    const short many = 20;
+    short many = 20;
+    short all_digits[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    short robot_array[5] = { 0, 0, 0, 0, 0 }, human_array[5] = { 0, 0, 0, 0, 0 };
+    int generate_number = 0, input_number;
+    short n, random_place;
+    short she_cow_count, he_cow_count;
+    int attempts = 0;
 
-    short all_numbers[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-    short robot_array[5] = { 0, 0, 0, 0, 0 };
-    short human_array[5] = { 0, 0, 0, 0, 0 };
-    int generate_number = 0;
-    int input_number = 0;
-    short n = 4, random_place;
-    short she_cow_count = 0, he_cow_count = 0;
-    int guesses = 0;
+    // ввод количества цифр
+    do
+    {
+        printf("Введите необходимое количество цифр (от 2 до 5): ");
+        scanf_s("%hd", &n);
+        printf("Вы должны вводить числа так, как того просит программа, иначе она не примет ваши значения. \nЕсли вы хотите закончить попытку, то введите отрицательное число.\n\nНачало игры:\n");
+        } while ((n < 2) || (n > 5));
     // генерация случайного n-значного числа
     for (int i = 0; i < n; i++)
     {
+        srand(time(0));
         back:
         random_place = rand() % 10;
-        if (all_numbers[random_place] < 10)
+        if (all_digits[random_place] < 10)
         {
             if ((i == 0) && (random_place == 0))
             {
                 robot_array[i] = 1;
-                all_numbers[1] = many;
+                all_digits[1] = many;
             }
             else
             {
-                robot_array[i] = all_numbers[random_place];
-                all_numbers[random_place] = many;
+                robot_array[i] = all_digits[random_place];
+                all_digits[random_place] = many;
             }
         }
         else
             goto back;
     }
-    //массив робота в сгенерированное число
     for (int k = 0; k < n; k++)
-        generate_number += robot_array[k] * pow(10, n - k - 1);
-    
-    //быки и коровы
-    while (he_cow_count < 4)
+        generate_number += robot_array[k] * (int)pow(10, n - k - 1);
+    //цикл игры
+    while (1)
     {
+        // обнуление результатов
         he_cow_count = 0;
         she_cow_count = 0;
-        guesses++;
-        printf("%d попытка: Введите %d-значное число: ", guesses, n);
-        scanf_s(" %d", &input_number);
-        //число человека в массив
-        for (int k = 0; k < n; k++)
+        // счётчик попыток
+        attempts++;
+        do
         {
-            human_array[n - k - 1] = input_number % 10;
-            input_number /= 10;
+            printf("%d попытка: Введите %d-значное число, где цифры не повторяются: ", attempts, n);
+            scanf_s("%d", &input_number);
+        } while (check_conditions(input_number, n));
+        if (input_number < 0)
+        {
+            printf("Ответ: %d\n", generate_number);
+            break;
         }
-        for (int i = 0; i < n; i++)
+        else
         {
-            for (int j = 0; j < n; j++)
+            //число человека в массив
+            for (int k = 0; k < n; k++)
             {
-                if (human_array[i] == robot_array[j])
+                human_array[n - k - 1] = input_number % 10;
+                input_number /= 10;
+            }
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
                 {
-                    if (i == j)
-                        he_cow_count++;
-                    else
-                        she_cow_count++;
+                    if (human_array[i] == robot_array[j])
+                    {
+                        if (i == j)
+                            he_cow_count++;
+                        else
+                            she_cow_count++;
+                        
+                    }
                 }
             }
+            if (he_cow_count == n)
+                break;
+            printf("Коров: %d\nБыков: %d\n", she_cow_count, he_cow_count);
         }
-        printf("Коров: %d\nБыков: %d\n", she_cow_count, he_cow_count);
-    } 
-    printf("\n**********************\n");
-    printf("Вы угадали");
-    printf("Количество попыток: %d\n", guesses);
-    printf("**********************\n\n");
+    }
+    if (input_number >= 0)
+    {
+        printf("\n**********************\n");
+        printf("Вы угадали!\n");
+        printf("Количество попыток: %d\n", attempts);
+        printf("**********************\n\n");
+    }
+    printf("\n");
     system("pause");
 }
