787f9479c03ccb54503c5b7eb468ef12f8dbf747
venn2713 <venn2713@gmail.com>
2022-10-12 13:17:25 +0300

    Task 3 with AMD Radeon Grapfic

--- vasilev_sa/task3/source.c
+++ vasilev_sa/task3/source.c
@@ -6,6 +6,7 @@
 #include <windows.h>
 #include "Header.h"
 
+#define many 20;
 
 int check_conditions(int input_number, short n, short count)
 {
@@ -36,10 +37,9 @@ int check_conditions(int input_number, short n, short count)
 
 void main()
 {
-  const short many = 20;
   short all_digits[10] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 }, robot_array[5] = { 0, 0, 0, 0, 0 }, human_array[5] = { 0, 0, 0, 0, 0 };
   int generate_number = 0, input_number, attempts = 0, time_x, time_y;
-  short n, random_place, she_cow_count, he_cow_count, count;
+  short n, random_place, cows, bulls, count;
   char ch;
   //начальные настройки
   setlocale(LC_ALL, "Russian");
@@ -51,50 +51,51 @@ void main()
   //----------------------------------------------------
   do
   {
-      clrscr();
-      gotoxy((window.Left + window.Right) / 4 + 1, (window.Top + window.Bottom) / 10);
-      textcolor(WHITE);
-      printf("Добро пожаловать в игру ");
-      textcolor(MAGENTA);
-      printf("\"Быки и коровы\"");
-      //----------------------------------------------------------------
-      textcolor(WHITE);
-      time_x = (window.Left + window.Right) / 20 - 1;
-      gotoxy(time_x, wherey() + 2);
-      printf("Цель игры - угадать число, состоящее из n - ого числа цифр.");
-      gotoxy(time_x, wherey() + 1);
-      printf("Программа загадывает число с неповторяющимися цифрами.");
-      gotoxy(time_x, wherey() + 1);
-      printf("Вы должны вводить числа, следую тому же правилу.");
-      gotoxy(time_x, wherey() + 1);
-      printf("Компьютер сообщит, сколько цифр угадано:");
-      gotoxy(time_x + 1, wherey() + 1);
-      printf("1) без совпадения с их позициями в загаданном числе (коровы).");
-      gotoxy(time_x + 1, wherey() + 1);
-      printf("2) вплоть до позиции в загаданном числе(быки).");
-      gotoxy(time_x, wherey() + 1);
-      textattr(RED);
-      printf("P.S: неверные значения программа не примет!");
-      gotoxy((window.Left + window.Right) / 6 - 2, (window.Top + window.Bottom) / 2);
-      //------------------------------------------------------------------
-      textcolor(GREEN);
-      printf("Введите необходимое количество цифр (от 2 до 5): ");
-      showcursor();
-      textcolor(WHITE);
-      count = scanf_s("%hd", &n);
-      while (ch = getchar() != '\n');
+    clrscr();
+    gotoxy((window.Left + window.Right) / 4 + 1, (window.Top + window.Bottom) / 10);
+    textcolor(WHITE);
+    printf("Добро пожаловать в игру ");
+    textcolor(MAGENTA);
+    printf("\"Быки и коровы\"");
+    //----------------------------------------------------------------
+    textcolor(WHITE);
+    time_x = (window.Left + window.Right) / 20 - 1;
+    gotoxy(time_x, wherey() + 2);
+    printf("Цель игры - угадать число, состоящее из n - ого числа цифр.");
+    gotoxy(time_x, wherey() + 1);
+    printf("Программа загадывает число с неповторяющимися цифрами.");
+    gotoxy(time_x, wherey() + 1);
+    printf("Вы должны вводить числа, следую тому же правилу.");
+    gotoxy(time_x, wherey() + 1);
+    printf("Компьютер сообщит, сколько цифр угадано:");
+    gotoxy(time_x + 1, wherey() + 1);
+    printf("1) без совпадения с их позициями в загаданном числе (коровы).");
+    gotoxy(time_x + 1, wherey() + 1);
+    printf("2) вплоть до позиции в загаданном числе (быки).");
+    gotoxy(time_x, wherey() + 1);
+    textattr(RED);
+    printf("P.S: неверные значения программа не примет!");
+    //------------------------------------------------------------------
+    gotoxy((window.Left + window.Right) / 6 - 2, (window.Top + window.Bottom) / 2);
+    textcolor(GREEN);
+    printf("Введите необходимое количество цифр (от 2 до 5): ");
+    showcursor();
+    textcolor(WHITE);
+    count = scanf_s("%hd", &n);
+    hidecursor();
+    while (ch = getchar() != '\n');
   } while ((n < 2) || (n > 5) || (count < 1));
   // generation
   for (int i = 0; i < n; i++)
   {
-      srand((unsigned int)time(NULL)); //может быть ошибка
+      srand((unsigned int)time(NULL));
       back:
       random_place = rand() % 10;
       if (all_digits[random_place] < 10)
       {
           if ((i == 0) && (random_place == 0))
           {
-              robot_array[i] = 1;
+              robot_array[0] = 1;
               all_digits[1] = many;
           }
           else
@@ -120,9 +121,9 @@ void main()
         if (attempts > 1)
         {
           gotoxy(time_x, time_y + 2);
-          printf("Коровы: %d", she_cow_count);
+          printf("Коровы: %d", cows);
           gotoxy(time_x, time_y + 3);
-          printf("Быки: %d", he_cow_count);
+          printf("Быки: %d", bulls);
         } 
         gotoxy(time_x, time_y);
         textcolor(GREEN);
@@ -130,18 +131,21 @@ void main()
         showcursor();
         textcolor(WHITE);
         count = scanf_s("%d", &input_number);
+        hidecursor();
         while (ch = getchar() != '\n');
     } while (check_conditions(input_number, n, count));
     if (input_number < 0)
     {
-        printf("Ответ: %d\n", generate_number);
-        break;
+      clrscr();
+      gotoxy(time_x, time_y);
+      textcolor(MAGENTA);
+      printf("Ответ: %d\n", generate_number);
+      break;
     }
-      
     else
     {
-      he_cow_count = 0;
-      she_cow_count = 0;
+      bulls = 0;
+      cows = 0;
       for (int k = 0; k < n; k++)
       {
         human_array[n - k - 1] = input_number % 10;
@@ -154,25 +158,26 @@ void main()
           if (human_array[i] == robot_array[j])
           {
             if (i == j)
-                he_cow_count++;
+                bulls++;
             else
-                she_cow_count++;            }
+                cows++;            
+          }
         }
       }
-      if (he_cow_count == n)
+      if (bulls == n)
         break;
-      printf("Коров: %d\nБыков: %d\n", she_cow_count, he_cow_count);
     }
   }
   if (input_number >= 0)
   {
     clrscr();
-    printf("\n**********************\n");
+    gotoxy(time_x - 3, time_y);
+    textcolor(MAGENTA);
     printf("Вы правы, ответ: %d\n", generate_number);
+    gotoxy(time_x - 4, time_y + 1);
     printf("Количество попыток: %d\n", attempts);
-    printf("**********************\n\n");
   }
-  printf("\n");
+  textcolor(BLACK);
   system("pause");
 }
 //int main()
