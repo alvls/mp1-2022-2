228650e2e676e06d6961f937cc3b5d8eb1ccbe6e
edlor_k <korablev.vlm@gmail.com>
2022-10-23 14:25:54 +0300

    task4 update 1

--- korablev_vm/task4/source.c
+++ korablev_vm/task4/source.c
@@ -5,6 +5,7 @@ int main()
 	int ch;
 	int c,a = 0;
 	setlocale(LC_ALL, "Rus");
+	system("title Онлайн-Касса");
 	ShowHelp(new);
 	List(new);
 	do {
@@ -15,17 +16,41 @@ int main()
 		GiveInfo(c);
 		ch = getchar();
 	} while (1);
-	if (c == 37) {
+	switch (c) {
+	case 37:
 		ShowHelp(1);
 		List(1);
 		new = 0;
 		ch = getchar();
 		main();
-	}
-	if (c == 38) {
+		break;
+	case 38:
 		printf("Спасибо за выбор нашей программы! Всего доброго!\n\n");
 		system("pause");
 		return 0;
+		break;
+	case 39:
+		showCheque();
+		system("cls");
+		do {
+			system("cls");
+			printf("Хотите ли вы сформировать новый чек? 1- да, 2 - нет: ");
+			scanf_s("%d", &a);
+		} while (a < 1 || a>2);
+		if (a == 1) {
+			Restart();
+			ch = getchar();
+			main();
+			new = 0;
+		}
+		else {
+			printf("\nСпасибо за выбор нашей программы! Всего доброго!\n\n");
+			system("pause");
+			return 0;
+		}
+		break;
+	default:
+		break;
 	}
 	if (c == 39) {
 		showCheque();
@@ -41,9 +66,10 @@ int main()
 			main();
 			new = 0;
 		}
-		else
+		else {
+			printf("\nСпасибо за выбор нашей программы! Всего доброго!\n\n");
+			system("pause");
 			return 0;
-
+		}
 	}
-	system("pause");
 }
