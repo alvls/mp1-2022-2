b7403571f18723f518a2d366f20033537b468d67
Кирилл Хасаньянов <hasanyanovk2@mail.ru>
2022-10-24 11:35:26 +0300

    Task 4 absolutely for check

--- hasanyanov_kv/task4/source.c
+++ hasanyanov_kv/task4/source.c
@@ -4,12 +4,12 @@
 #include <time.h>
 #include <math.h>
 #include "Header.h"
-#define SIZE 12
+#define SIZE 13
 
-const char* KODES[SIZE] = {"1234", "2345", "3456", "4567", "5678", "6789", "7890", "6534", "3556", "0000", "0110", "0123"};
-const char* NAMES[SIZE] = { "Кружка пивная     ", "Ручка шариковая   ", "Клавиатура        ", "Стол письменный   ","Рюкзак школьный   ","Портмоне кожаное  ","Морозильная камера","Монитор IPS       ", "Мяч волейбольный  ", "Кроссовки беговые ", "Наушники          ", "Куртка зимняя     "};
-const int COSTS[SIZE] = { 300, 35, 8200, 12000, 3000, 1550, 18500, 31000, 7800, 5700, 16500, 9000 };
-const int DISCOUNTS[SIZE] = { 10, 5, 20, 15, 10, 5, 25, 30, 35, 50, 40, 45 };
+const char* KODES[SIZE] = {"1234", "2345", "3456", "4567", "5678", "6789", "7890", "6534", "3556", "0000", "0110", "0123", "5437"};
+const char* NAMES[SIZE] = { "Кружка пивная", "Ручка шариковая", "Клавиатура", "Стол письменный","Рюкзак школьный","Портмоне кожаное","Морозильная камера","Монитор IPS", "Мяч волейбольный  ", "Кроссовки беговые ", "Наушники", "Куртка зимняя", "Кольцо серебряное"};
+const int COSTS[SIZE] = { 300, 35, 8200, 12000, 3000, 1550, 18500, 31000, 7800, 5700, 16500, 9000, 7000 };
+const int DISCOUNTS[SIZE] = { 10, 5, 20, 15, 10, 5, 25, 30, 35, 50, 40, 45, 10 };
 int kol[SIZE] = { 0 };
 
 int find_ind(char* x)
@@ -62,11 +62,12 @@ int main()
 		gotoxy(0, 2);
 		printf("================================================================================\n");
 		printf("Наш ассортимент:");
-		printf("\n%-25s %-20s %-5s\n", "Штрих-код:", "Наименование:", "Цена(скидка):\n");
+		printf("\n%-25s %-20s %-5s\n", "Штрих-код:", "Наименование:", "Цена (скидка):\n");
 		for (int k = 0; k < SIZE; k++)
 		{
-			printf("%s			  %s   %d(%d%%)\n", KODES[k], NAMES[k], COSTS[k], DISCOUNTS[k]);
+			printf("%-25s %-20s %-5d (%d%%)\n", KODES[k], NAMES[k], COSTS[k], DISCOUNTS[k]);
 		}
+
 		printf("================================================================================\n");
 		gotoxy(buf.X / 2 - 12, (buf.Y / 2) + 2);
 		printf("Введите цифры штрих-кода:");
@@ -91,7 +92,9 @@ int main()
 			scanf_s(" %c", &znak);
 			clrscr();
 			gotoxy(buf.X / 2 - 12, (buf.Y / 2) + 2);
-			if (znak == '$')
+			switch (znak)
+			{
+			case'$':
 			{
 				mode = 0;
 				kol[ind]++;
@@ -111,25 +114,25 @@ int main()
 					{
 						count++;
 						summ += COSTS[i] * kol[i] - COSTS[i] * kol[i] * DISCOUNTS[i] / 100;
-						
 
-						gotoxy(buf.X / 2 - 31.5, (buf.Y/2) + count + 2);
+
+						gotoxy(buf.X / 2 - 31.5, (buf.Y / 2) + count + 2);
 						printf("%d)", count);
-						gotoxy(buf.X/2 - 31.5 + 3, (buf.Y / 2) + count + 2);
+						gotoxy(buf.X / 2 - 31.5 + 3, (buf.Y / 2) + count + 2);
 						printf("%s", NAMES[i]);
-						gotoxy(buf.X/2 - 31.5 + 22, (buf.Y / 2) + count + 2);
+						gotoxy(buf.X / 2 - 31.5 + 22, (buf.Y / 2) + count + 2);
 
 						printf("%dруб.", COSTS[i]);
 
-						gotoxy(buf.X/2 - 31.5 + 38.5, (buf.Y / 2) + count + 2);
+						gotoxy(buf.X / 2 - 31.5 + 38.5, (buf.Y / 2) + count + 2);
 
 						printf("%d", kol[i]);
 
-						gotoxy(buf.X/2 - 31.5 + 48.5, (buf.Y / 2) + count + 2);
+						gotoxy(buf.X / 2 - 31.5 + 48.5, (buf.Y / 2) + count + 2);
 
 						printf("%d%%", DISCOUNTS[i]);
 
-						gotoxy(buf.X/2 - 31.5 + 57, (buf.Y / 2) + count + 2);
+						gotoxy(buf.X / 2 - 31.5 + 57, (buf.Y / 2) + count + 2);
 
 						printf("%dруб.", COSTS[i] * kol[i] - COSTS[i] * kol[i] * DISCOUNTS[i] / 100);
 
@@ -139,21 +142,24 @@ int main()
 				printf("ИТОГО К ОПЛАТЕ: %gруб.", summ);
 				gotoxy(buf.X / 2 - 33, (buf.Y / 2) + 4 + count);
 				printf("**********************************************************************");
-			}
-			else if(znak == '-')
+				break;
+			}			
+			case '-':
 			{
-				;
-			}
-			else if(znak == '+')
+				break;
+			}			
+			case '+':
 			{
-				kol[ind]++;;
+				kol[ind]++;
+				break;
 			}
-			else
+			default:
 			{
 				gotoxy(buf.X / 2 - 6.5, (buf.Y / 2) + 0);
 				printf("Неверный знак\n");
 				gotoxy(0, 0);
 				system("PAUSE");
+			}
 			}
 		
 		}
