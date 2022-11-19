18dea3d5334aa1cdd79490cc636e2b4c365738b1
KorneevaEkaterina <e.korneeva1917@gmail.com>
2022-11-19 18:42:13 +0300

    Task 5 complete

--- korneeva_em/task5/sort_algorithms.h
+++ korneeva_em/task5/sort_algorithms.h
@@ -3,12 +3,12 @@
 #include <stdio.h>  
 #include <io.h>  
 
-typedef void (*SortAlgorithm)(struct _finddata_t* arr, size_t len, int asc);
+typedef void (*SortAlgorithm)(struct _finddata_t* arr, size_t* iarr, size_t len, int asc);
 
-void bubble_sort(struct _finddata_t* arr, size_t len, int asc);
-void select_sort(struct _finddata_t* arr, size_t len, int asc);
-void insert_sort(struct _finddata_t* arr, size_t len, int asc);
-void merge_sort(struct _finddata_t* arr, size_t len, int asc);
-void quick_sort(struct _finddata_t* arr, size_t len, int asc);
-void increment_sort(struct _finddata_t* arr, size_t len, int asc);
-void count_sort(struct _finddata_t* arr, size_t len, int asc);
+void bubble_sort(struct _finddata_t* arr, size_t* iarr, size_t len, int asc);
+void select_sort(struct _finddata_t* arr, size_t* iarr, size_t len, int asc);
+void insert_sort(struct _finddata_t* arr, size_t* iarr, size_t len, int asc);
+void merge_sort(struct _finddata_t* arr, size_t* iarr, size_t len, int asc);
+void quick_sort(struct _finddata_t* arr, size_t* iarr, size_t len, int asc);
+void increment_sort(struct _finddata_t* arr, size_t* iarr, size_t len, int asc);
+void count_sort(struct _finddata_t* arr, size_t* iarr, size_t len, int asc);
