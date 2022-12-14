0ade1146660485546b8b935133f7b3bee71c9f3d
Иван Капустин <ivankapustin60@gmail.com>
2022-12-15 02:16:45 +0300

    task 6.1

--- kapustin_ia/task6/funk.c
+++ kapustin_ia/task6/funk.c
@@ -21,7 +21,7 @@ double sinus(double x0, double epsilon, int n1, int* count)
 		sum1 = sum1 + swap_sign * row_elem;
 		if (epsilon == 0)
 		{
-			printf("%-5d                                         %-5.15lf                                              %-5.15lf\n", count_iteration + 1, sum1, sum1 - sin(x0));
+			printf("%-5d                                         %-5.20lf                                              %-8.20lf\n", count_iteration + 1, sum1, sum1 - sinl(x0));
 		}
 		row_elem = row_elem * (x0 / (coef + 1)) * (x0 / (coef + 2));
 		coef = coef + 2;
@@ -34,17 +34,18 @@ double sinus(double x0, double epsilon, int n1, int* count)
 
 double cosinus(double x0, double epsilon, int n1, int* count)
 {
-	double sum1 = 0, sum2 = 10, row_elem;
+	double sum1 = 0, sum2 = 10, row_elem, etal;
 	int swap_sign = 1, count_eteration = 0, coef = 0;
 	row_elem = 1;
 	x0 = x0 * 3.14 / 180;
+	etal = cosl(x0);
 	while (fabs(sum1 - sum2) >= epsilon && count_eteration <= n1)
 	{
 		sum2 = sum1;
 		sum1 = sum1 + swap_sign * row_elem;
 		if (epsilon == 0)
 		{
-			printf("%-5d                                         %-5.15lf                                              %-5.15lf\n", count_eteration + 1, sum1, sum1 - sin(x0));
+			printf("%-5d                                         %-5.20lf                                              %-8.20lf\n", count_eteration + 1, sum1, sum1 - etal);
 		}
 		row_elem = row_elem * (x0 / (coef + 1)) * (x0 / (coef + 2));
 		coef = coef + 2;
@@ -71,7 +72,7 @@ double expo(double x0, double epsilon, int n1, int* count)
 		sum1 = sum1 + row_elem;
 		if (epsilon == 0)
 		{
-			printf("%-5d                                         %-5.15lf                                              %30.15lf\n", count_eteration , sum1, sum1 - sin(x0));
+			printf("%-5d                                         %-5.20lf                                                   %-8.20lf \n", count_eteration , sum1, sum1 - exp(x0));
 		}
 		row_elem = row_elem * (x0 / (coef + 1));
 		coef = coef + 1;
@@ -100,7 +101,7 @@ double arccos(double x0, double epsilon, int n1, int* count)
 			sum1 = sum1 + row_elem;
 			if (epsilon == 0)
 			{
-				printf("%-5d                                         %-5.15lf                                              %-5.15lf\n", count_eteration + 1, sum1, sum1 - sin(x0));
+				printf("%-5d                                         %-5.20lf                                              %-8.20lf\n", count_eteration + 1, sum1, sum1 - acosl(x0));
 			}
 			row_elem = row_elem * (x0 / (coef + 1)) * (x0 / (coef + 2)) * coef * coef;
 			coef = coef + 2;
